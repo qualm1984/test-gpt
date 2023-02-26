799767e49141e69dadda571a1ba5c7afa9426b14
Rob Francis <robertfrancis1984@gmail.com>
2023-02-26 19:51:35 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def open_file(filepath):
         return infile.read()
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['VMware Support:', 'USER:']):
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.0, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['VMware Support:', 'USER:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
