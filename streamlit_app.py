f056348d23f23e0ea8a0e9a1cc09c36b30e2837e
Rob Francis <robertfrancis1984@gmail.com>
2023-02-26 19:53:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,16 @@ import streamlit as st
 import openai
 
 
-def open_file(filepath):
-    if 'openai' in st.secrets:
-        return st.secrets['openai']['OPENAI_API_KEY']
-    with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
+def get_api_key():
+    return os.environ.get('OPENAI_API_KEY')
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.0, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['VMware Support:', 'USER:']):
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['VMware Support:', 'USER:']):
+    api_key = get_api_key()
+    if api_key is None:
+        st.error('Please set the OPENAI_API_KEY environment variable')
+        return
+
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -21,7 +23,7 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.0, top_p=1.0, toke
         frequency_penalty=freq_pen,
         presence_penalty=pres_pen,
         stop=stop,
-        OPENAI_API_KEY=st.secrets["openai"]["OPENAI_API_KEY"])
+        api_key=api_key)
     text = response['choices'][0]['text'].strip()
     return text
 
