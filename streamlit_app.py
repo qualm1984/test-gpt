a1bbd51bb7ed8cddacdf1213b41860873b31253c
Rob Francis <robertfrancis1984@gmail.com>
2023-02-26 19:58:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,10 @@ import openai
 def get_api_key():
     return os.environ.get('OPENAI_API_KEY')
 
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
 
 def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['VMware Support:', 'USER:']):
     api_key = get_api_key()
