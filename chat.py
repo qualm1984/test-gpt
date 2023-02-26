739003ce615641beabb39afb119a584457d53e9a
Rob Francis <robertfrancis1984@gmail.com>
2023-02-26 21:17:49 +0000

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,17 +1,23 @@
+import os
 import streamlit as st
-from streamlit_chat import message
 import openai
 
+def get_api_key():
+    api_key = st.secrets["openai"]["api_key"]
+    if api_key is None:
+        st.error('Please set the OPENAI_API_KEY environment variable')
+        return
+    return api_key
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
-
-openai.api_key = open_file('openaiapikey.txt')
-
-
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['VMware Support:', 'USER:']):
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0):
+    api_key = get_api_key()
+    if api_key is None:
+        st.error('Please set the OPENAI_API_KEY environment variable')
+        return
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -21,19 +27,24 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
         top_p=top_p,
         frequency_penalty=freq_pen,
         presence_penalty=pres_pen,
-        stop=stop)
+        api_key=api_key)
     text = response['choices'][0]['text'].strip()
     return text
 
-
-if __name__ == '__main__':
+def main():
+    st.title('VMware Support Assistant')
     conversation = list()
     while True:
-        user_input = input('USER: ')
+        user_input = st.text_input('USER: ')
+        if not user_input:
+            continue
         conversation.append('USER: %s' % user_input)
         text_block = '\n'.join(conversation)
         prompt = open_file('prompt_chat.txt').replace('<<BLOCK>>', text_block)
         prompt = prompt + '\nVMware Support:'
         response = gpt3_completion(prompt)
-        print('VMware Support:', response)
+        st.write('VMware Support:', response)
         conversation.append('VMware Support: %s' % response)
+
+if __name__ == '__main__':
+    main()
